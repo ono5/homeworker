76caa0f03ae1fdb0a241d48d4fc7ce3686a8e45c
ono5 <kuroneko_head_lord@yahoo.co.jp>
2019-03-31 22:49:33 +0900

    feat: Added Pagination for our blog app.

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 
 from .models import Post
@@ -6,10 +6,25 @@ from .models import Post
 
 def post_list(request):
     """Show All Post"""
-    posts = Post.published.all()
+    object_list = Post.published.all()
+    # Instantiate the Paginator class with the number of objects we want to display on each page.
+    paginator = Paginator(object_list, 3) # 3 posts in each page
+    # indeicate the current page number
+    current_page = request.GET.get('page')
+    try:
+        # Obtain the objects for the desired page
+        posts = paginator.page(current_page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'page': current_page, # page number
+                   'posts': posts}) # retrieved object
 
 
 def post_detail(request, year, month, day, post):
